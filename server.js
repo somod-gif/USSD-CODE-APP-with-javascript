0fc83f3a20002d6707da6f2d78dc907b552420e9
somod-gif <eniolabadmus351@gmail.com>
2025-04-30 17:12:25 +0100

    Fix route wildcard issue

--- server.js
+++ server.js
@@ -1,52 +1,50 @@
-const app = require('express')()
-const bodyParser = require('body-parser')
-const logger = require('morgan')
+const express = require('express');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
 
-const port = process.env.PORT || 5000
+const app = express();
+const port = process.env.PORT || 5000;
 
-app.use(logger('dev'))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
+// Middlewares
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('*', (req, res) => {
-  res.send('This is a tutorial for creating a USSD app with javascript within 10minutes')
-})
+// GET route (for test)
+app.get('/', (req, res) => {
+  res.send("USSD app using JavaScript and Africa's Talking API");
+});
 
-app.post('*', (req, res) => {
-  let {sessionId, serviceCode, phoneNumber, text} = req.body
-  if (text == '') {
-    // This is the first request. Note how we start the response with CON
-    let response = `CON What would you want to check
-    1. My Account
-    2. My phone number`
-    res.send(response)
-  } else if (text == '1') {
-    // Business logic for first level response
-    let response = `CON Choose account information you want to view
-    1. Account number
-    2. Account balance`
-    res.send(response)
-  } else if (text == '2') {
-    // Business logic for first level response
-    let response = `END Your phone number is ${phoneNumber}`
-    res.send(response)
-  } else if (text == '1*1') {
-    // Business logic for first level response
-    let accountNumber = 'ACC1001'
-    // This is a terminal request. Note how we start the response with END
-    let response = `END Your account number is ${accountNumber}`
-    res.send(response)
-  } else if (text == '1*2') {
-    // This is a second level response where the user selected 1 in the first instance
-    let balance = 'NGN 10,000'
-    // This is a terminal request. Note how we start the response with END
-    let response = `END Your balance is ${balance}`
-    res.send(response)
+// POST route (for USSD callback)
+app.post('/ussd', (req, res) => {
+  const { sessionId, serviceCode, phoneNumber, text } = req.body;
+  let response = '';
+
+  if (text === '') {
+    response = `CON What would you want to check
+1. My Account
+2. My phone number`;
+  } else if (text === '1') {
+    response = `CON Choose account information you want to view
+1. Account number
+2. Account balance`;
+  } else if (text === '2') {
+    response = `END Your phone number is ${phoneNumber}`;
+  } else if (text === '1*1') {
+    const accountNumber = 'ACC1001';
+    response = `END Your account number is ${accountNumber}`;
+  } else if (text === '1*2') {
+    const balance = 'NGN 10,000';
+    response = `END Your balance is ${balance}`;
   } else {
-    res.status(400).send('Bad request!')
+    response = 'END Invalid input';
   }
-})
 
+  res.set('Content-Type', 'text/plain');
+  res.send(response);
+});
+
+// Start server
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`)
-})
+  console.log(`Server running on port ${port}`);
+});
